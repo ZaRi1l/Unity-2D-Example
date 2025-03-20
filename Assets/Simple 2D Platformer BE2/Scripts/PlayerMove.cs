574e79d838ed41b2cbaba15a16ea0ae64dd9220b
ZaRi <zari_09@naver.com>
2025-03-20 20:58:20 +0900

    골드메탈 b15
    b15 까지 들음

---
+++ Assets/Simple 2D Platformer BE2/Scripts/PlayerMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    public float maxSpeed;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(0.5f * rigid.velocity.normalized.x, rigid.velocity.y);
+        }
+
+        // Direction Sprite
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        // Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+        {
+            anim.SetBool("isWalking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        // Move by key control
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        // Max Speed
+        if (rigid.velocity.x > maxSpeed)
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < -maxSpeed)
+        {
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+        }
+    }
+}
+

eda6b4b082ee11335a68fa8dc86765d1a3617023
ZaRi <zari_09@naver.com>
2025-03-22 22:10:20 +0900

    monster AI commit
    기초 강좌 6번째까지

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,7 @@ public class PlayerMove : MonoBehaviour
     Animator anim;
 
     public float maxSpeed;
+    public float jumpPower;
 
     void Start()
     {
@@ -20,6 +21,14 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        // Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
+
         // Stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
@@ -59,6 +68,20 @@ public class PlayerMove : MonoBehaviour
         else if (rigid.velocity.x < -maxSpeed)
         {
             rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+        }
+
+        // Landing Platform
+        if(rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping", false);
+            }
         }
     }
 }
